319dbca46b730754c0eeb9d45985885b4312f532
Andrew N. Smith <asmith@owlshares.com>
2024-06-04 22:38:23 -0400

    add bootstrap configuration, and add config attributes for non boostrap configuations to provide a boostrap id and bootstrap address as required environment variables

--- src/server.rs
+++ src/server.rs
@@ -38,17 +38,65 @@ async fn main() -> std::io::Result<()> {
     let (_stop_tx, mut stop_rx) = tokio::sync::mpsc::channel(1024);
     let pd_endpoints = vec!["127.0.0.1:2379"];
 
-    let event_broker = Arc::new(Mutex::new(EventBroker::new()));
-    let network_state = Arc::new(RwLock::new(AllegraNetworkState::new()));
+    let event_broker = Arc::new(
+        Mutex::new(
+            EventBroker::new()
+        )
+    );
+
+
+    let mut network_state = Arc::new(
+        RwLock::new(
+            AllegraNetworkState::new(
+                event_broker.clone()
+            )
+        )
+    );
+
+    let mut guard = network_state.write().await; 
+    guard.add_peer(&local_peer).await?;
+
+    #[cfg(not(feature="bootstrap"))]
+    {
+        let bootstrap_addr = std::env::var(
+            "BOOTSTRAP_ADDR"
+        ).expect(
+            "If not configured as bootsrap node, bootstrap node is required"
+        );
+        let bootstrap_id = std::env::var(
+            "BOOTSTRAP_ID"
+        ).expect(
+            "If not configured as boostrap node, bootstrap node is required"
+        ); 
+        let bootstrap_peer = Peer::new(
+            uuid::Uuid::parse_str(
+                &bootstrap_id
+            ).expect(
+                "bootstrap_id must be valid uuid v4"
+            ), bootstrap_addr
+        );
+        guard.add_peer(&bootstrap_peer).await?;
+    }
+    drop(guard);
+
     let mut vmm = VmManager::new(
-        pd_endpoints, None, 2223, event_broker.clone()
+        pd_endpoints,
+        None,
+        2222,
+        event_broker.clone()
     ).await?;
 
     let task_cache = vmm.task_cache();
+
     let vmm_handle = tokio::task::spawn(async move {
-        let _ = vmm.run(&mut rx, &mut stop_rx).await;
+        let _ = vmm.run(
+            &mut rx,
+            &mut stop_rx
+        ).await;
     });
+
     let pd_endpoints = vec!["127.0.0.1:2379"];
+
     let tikv_client = tikv_client::RawClient::new(
         pd_endpoints
     ).await.map_err(|e| {
@@ -58,7 +106,7 @@ async fn main() -> std::io::Result<()> {
         )
     })?;
 
-    let next_port = 2223;
+    let next_port = 2222;
 
     let service = VmmService {
         local_peer,
@@ -82,7 +130,11 @@ async fn main() -> std::io::Result<()> {
         )
     })?;
 
-    Server::builder().add_service(vmmserver).serve(addr).await.map_err(|e| {
+    Server::builder().add_service(
+        vmmserver
+    ).serve(
+        addr
+    ).await.map_err(|e| {
         std::io::Error::new(
             std::io::ErrorKind::Other,
             e
@@ -90,5 +142,6 @@ async fn main() -> std::io::Result<()> {
     })?;
 
     vmm_handle.await?;
+
     Ok(())
 }
