6b6fe9b2736285eb93b428b2926174b4e137f455
Andrew N. Smith <asmith@owlshares.com>
2024-06-15 18:11:52 -0400

    add monitor back in but leave event handler out

--- src/server.rs
+++ src/server.rs
@@ -180,7 +180,6 @@ async fn main() -> std::io::Result<()> {
     #[cfg(not(feature="bootstrap"))]
     drop(guard);
 
-    /*
     let (_stop_tx, stop_rx) = std::sync::mpsc::channel();
     log::info!("created channel");
     let queue = Arc::new(
@@ -190,11 +189,14 @@ async fn main() -> std::io::Result<()> {
     );
     log::info!("created queue");
 
+    /*
     let event_handling_queue = queue.clone();
     log::info!("initiated event handling queue");
     let handle_monitor_events = tokio::spawn(async move {
         handle_events(event_handling_queue.clone()).await;
     });
+    */
+
     log::info!("started monitor event handler thread");
     let directory_to_monitor = std::env::var("LXD_STORAGE_DIR").unwrap_or_else(|_| {
         DEFAULT_LXD_STORAGE_DIR.to_string()
@@ -207,7 +209,6 @@ async fn main() -> std::io::Result<()> {
     });
 
     log::info!("started monitor directory thread");
-    */
     let (tx, mut rx) = tokio::sync::mpsc::channel(1024);
 
     let next_port = 2222;
@@ -257,7 +258,7 @@ async fn main() -> std::io::Result<()> {
     })?;
     // vmm_handle.await?;
     //handle_monitor_events.await;
-    //monitor_directory.await;
+    monitor_directory.await;
 
     Ok(())
 }
