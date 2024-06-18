940dd1e06d7bdd1930227c5d2a5a476700c99d97
Andrew N. Smith <asmith@owlshares.com>
2024-06-18 01:18:35 -0400

    add fs_monitor_service

--- src/fs_monitor_service.rs
+++ src/fs_monitor_service.rs
@@ -1,4 +1,43 @@
+use libretto::watcher::monitor_directory;
+use libretto::client::handle_events;
+use std::sync::Arc;
+use tokio::sync::RwLock;
+use std::collections::VecDeque;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    pub static ref DEFAULT_LXD_STORAGE_POOL: &'static str = "/mnt/libretto/lxd-storage-pool"; 
+}
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let (_stop_tx, stop_rx) = std::sync::mpsc::channel();
+    let queue = Arc::new(
+        RwLock::new(
+            VecDeque::new()
+        )
+    );
+
+    let event_handling_queue = queue.clone();
+    let event_handler = tokio::spawn(async move {
+        handle_events(event_handling_queue.clone()).await;
+    });
+
+    let dir = std::env::var(
+        "LXD_STORAGE_POOL"
+    ).unwrap_or_else(|_| {
+        DEFAULT_LXD_STORAGE_POOL.to_string()
+    });
+    dbg!(&dir);
+    let monitor = tokio::spawn(async move {
+        let _ = monitor_directory(
+            &dir,
+            queue,
+            stop_rx
+        ).await;
+    });
+
+    let _ = tokio::join!(monitor, event_handler);
+
     Ok(())
 }
