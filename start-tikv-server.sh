5902d684b741f33de996f6c1f4ccdea8ce19a594
Andrew N. Smith <asmith@owlshares.com>
2024-07-01 18:33:02 -0400

    use HOST_IP instead of localhost for pd in tikv-server startup script

--- start-tikv-server.sh
+++ start-tikv-server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+HOST_IP=$(ip route | awk '/default/ { print $3 }')
+
 # Check if tikv-server container is running
 if [ "$(docker ps -q -f name=tikv-server)" ]; then
     echo "tikv-server is already running."
@@ -13,9 +15,9 @@ else
         # Run a new tikv-server container
         echo "Running new tikv-server container..."
         docker run -d --name tikv-server --network host pingcap/tikv:latest \
-            --addr="127.0.0.1:20160" \
-            --advertise-addr="127.0.0.1:20160" \
+            --addr="0.0.0.0:20160" \
+            --advertise-addr="0.0.0.0:20160" \
             --data-dir="/tikv" \
-            --pd="http://127.0.0.1:2379"
+            --pd="http://$HOST_IP:2379"
     fi
 fi
